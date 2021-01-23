5156a9da587740fc904358fc3e6bb093324a0931
Amita Date <amita.date@weboniselab.com>
2021-01-24 00:55:08 +0530

    1. Handling Birth Year validation. Changed the regex for checking valid year 2. Added few more validations wrt yes/no option selection 3. Added strip() to all raw_input() to remove spaces 4. Updated Error messages

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -6,43 +6,43 @@ yes = set(['yes', 'y', 'ye'])
 no = set(['no', 'n'])
 mylist = []
 
-userName = raw_input("Enter the username: ")
+userName = raw_input("Enter the username: ").strip()
 if not re.match("^[a-z]+$", userName):
-    sys.exit("Error! Only letters a-z allowed!")
+    sys.exit("Error! Only alphabets are allowed!")
 else:
     mylist.append(userName)
-    birthYear = raw_input("Enter the your birth year: ")
-    if not re.match('^[0-9]+$', birthYear):
-        sys.exit("Error! Only numbers 0-9 allowed!")
+    birthYear = raw_input("Enter the your birth year: ").strip()
+    if not re.match('^(19|20)\\d{2}$', birthYear):
+        sys.exit("Error! Please enter valid birth year.")
     else:
         mylist.append(birthYear)
         choice = raw_input("Do you have a pet?" + "[Yes/No]")
-        userchoice = choice.lower()
-        if userchoice.isspace() or userchoice == "":
-            sys.exit("Please enter proper choice. Spaces are not allowed")
+        userchoice = choice.lower().strip()
+        if userchoice.isspace() or userchoice == "" or userchoice not in yes and userchoice not in no:
+            sys.exit("Please enter proper choice.")
         if userchoice in yes:
-            petName = raw_input("Enter the pet name: ")
+            petName = raw_input("Enter the pet name: ").strip()
             if re.match("^[a-z]+$", petName):
                 mylist.append(petName)
             else:
-                sys.exit("Error! Only letters a-z allowed!")
+                sys.exit("Error! Only alphabets are allowed!")
         else:
-            choice1 = raw_input("Do you have a car?" + "[Enter car brand name if Yes]" + "[Yes/No]")
-            userchoice1 = choice1.lower()
-            if userchoice1.isspace() or userchoice1 == "":
-                sys.exit("Please enter proper choice. Spaces are not allowed")
+            choice1 = raw_input("Do you have a car?" + "[Yes/No]")
+            userchoice1 = choice1.lower().strip()
+            if userchoice1.isspace() or userchoice1 == "" or userchoice1 not in yes and userchoice1 not in no:
+                sys.exit("Please enter proper choice.")
             if userchoice1 in yes:
-                brandName = raw_input("Enter the brand name: ")
+                brandName = raw_input("Enter the brand name: ").strip()
                 if re.match("^[a-z]+$", brandName):
                     mylist.append(brandName)
                 else:
-                    sys.exit("Error! Only letters a-z allowed!")
+                    sys.exit("Error! Only alphabets are allowed!")
             else:
-                cityName = raw_input("Enter your city name")
+                cityName = raw_input("Enter your city name: ").strip()
                 if re.match("^[a-z]+$", cityName):
                     mylist.append(cityName)
                 else:
-                    sys.exit("Error! Only letters a-z allowed!")
+                    sys.exit("Error! Only alphabets are allowed!")
 
 random.shuffle(mylist)
 print(mylist)
